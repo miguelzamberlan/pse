5a1eed21a16bc8e4cd23c6e6eaf5e0feb5a0798e
Miguel Zamberlan <miguel.zamberlan@gmail.com>
2019-09-02 11:41:04 -0400

    ajustes

--- manage.py
+++ manage.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-
-def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pse.settings')
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'psmg.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,6 +14,3 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-
-if __name__ == '__main__':
-    main()
